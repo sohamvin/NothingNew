f66169b006d37a7a6342f8fb5355667d84f703a6
sohamvin <doyalkudo@gmail.com>
2025-01-29 16:51:56 +0530

    Done

--- activate.sh
+++ activate.sh
@@ -1,4 +1,5 @@
 cwd=$(pwd)
-python3 "$cwd/VTwo/WorkerTwo.py"
-python3 "$cwd/VTwo/PostProcessorTwo.py"
-python3 "$cwd/main.py"
+python3 "$cwd/VTwo/WorkerTwo.py" &
+python3 "$cwd/VTwo/PostProcessorTwo.py" &
+python3 "$cwd/main.py" &
+wait
